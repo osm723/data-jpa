ec6e69ba1be918260e2fa00bd825c9c195a330a4
oh <osm723@gmail.com>
2024-06-02 21:37:41 +0900

    Initial commit

---
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -0,0 +1,41 @@
+package study.data_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+//    protected Member() {
+//    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        this.team = team;
+    }
+
+    public void chageTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
